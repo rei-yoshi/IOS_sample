5a5b50cb1f007dc39d4a9a39271efb5ca7e763ad
rei-yoshi <rei.yosida.5a@stu.hosei.ac.jp>
2019-06-17 00:13:09 +0900

    0616

--- IOS_sample/GameScene.swift
+++ IOS_sample/GameScene.swift
@@ -9,81 +9,68 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene , SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var ball:SKSpriteNode!
+    var paddle:SKSpriteNode!
     
     override func didMove(to view: SKView) {
+        //withname にはGameScene.sksで作成した名前
+        ball   = self.childNode(withName: "ball") as? SKSpriteNode
+        paddle = self.childNode(withName: "paddle") as? SKSpriteNode
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        //ボールの飛んでいく方向
+        ball.physicsBody?.applyImpulse(CGVector(dx: 50, dy: 50))
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        //壁にボールがぶつかった時、跳ねかえる
+        let border = SKPhysicsBody(edgeLoopFrom: (view.scene?.frame)!)
+        border.friction = 0
+        self.physicsBody = border
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        //SKPhysicsContactDelegate をクラス参照に入れてないとエラーとなる
+        self.physicsWorld.contactDelegate = self
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    //パドル
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        for touch in touches
+        {
+            let toucLocation = touch.location(in: self)
+            paddle.position.x = toucLocation.x
+        }
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    //パドル
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        for touch in touches
+        {
+            let toucLocation = touch.location(in: self)
+            paddle.position.x = toucLocation.x
+        }
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    //ボールがブロックに当たると消える。
+    func didBegin(_ contact : SKPhysicsContact)
+    {
+        let bodyAName = contact.bodyA.node?.name;
+        let bodyBName = contact.bodyB.node?.name;
+        
+        if bodyAName == "ball" && bodyBName == "brick" ||
+            bodyAName == "brick" && bodyBName == "ball"
+        {
+            if bodyAName == "brick"
+            {
+                contact.bodyA.node?.removeFromParent()
+            }
+            else if bodyBName == "brick"
+            {
+                contact.bodyB.node?.removeFromParent()
+            }
+        }
     }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+        
 }
+
